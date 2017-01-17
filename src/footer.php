9bed38245d89a197838da2cd3d6b7d31b4cb1bd3
Anthony <anthonynguyen77@hotmail.fr>
2017-01-17 15:48:04 +0100

    Petites corrections et ajouts de liens footer

--- src/footer.php
+++ src/footer.php
@@ -2,13 +2,13 @@
             <div class="grid_12">
                 <footer>
                     <div class="socials">
-                        <a href="#">facebook</a> | 
-                        <a href="#">twitter</a> | 
-                        <a href="#">google+</a>
+                        <a href="https://www.facebook.com/pablo.gutierrez.902604?fref=ts">Facebook</a> | 
+                        <a href="https://twitter.com/karma_police94">Twitter</a> | 
+                        <a href="#">Google+</a>
                     </div>
                     <div class="copy">
-                        lawyers bureau &copy; 2013 | <a href="index-5.php">Privacy Policy</a>
-                        More  at <a rel="nofollow" href="http://www.templatemonster.com/category.php?category=0&amp;type=1" target="_blank">TemplateMonster.com</a>
+                        E-commerce Nguttierez | <a href="index-5.php">Privacy Policy</a>
+                        More  at <a rel="nofollow" href="#" target="_blank"> A propos</a>
                     </div>
                 </footer>
             </div>
